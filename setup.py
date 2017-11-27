16ac865de29406c2d0ce76cbee38450db858bb1b
Jeremy Singer-Vine <jsvine@gmail.com>
2017-11-26 23:55:40 -0500

    Pin colormath and networkx requirements
    Fixes issue #8.

--- setup.py
+++ setup.py
@@ -22,9 +22,11 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3"
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ],
     keywords="color colors colorspace scale spectrum",
     author="Jeremy Singer-Vine",
